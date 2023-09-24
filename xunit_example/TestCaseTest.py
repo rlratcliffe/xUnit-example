9171ab53a189e567437bd537054982ff77d35385
Rob Ratcliffe <rlr@tcliffe.dev>
2023-09-24 11:21:34 -0400

    should pass
    - using main class isn't in the book but not sure how to get poetry to run a script without a main method yet

--- xunit_example/TestCaseTest.py
+++ xunit_example/TestCaseTest.py
@@ -4,6 +4,6 @@ from xunit_example.TestCase import TestCase
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        # assert not test.wasRun
-        # test.run()
+        assert not test.wasRun
+        test.run()
         assert test.wasRun
